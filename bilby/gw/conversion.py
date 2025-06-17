e3e74ce69a78ef7376155873e2dc6b704667889c
Noah Hopkins <hopkins1@kenyon.edu>
2025-06-17 15:16:59 -0400

    Second attempt at pressure conversions. Should return now.

--- bilby/gw/conversion.py
+++ bilby/gw/conversion.py
@@ -286,12 +286,18 @@ def convert_to_lal_binary_black_hole_parameters(parameters):
 
 
 def generate_component_masses_from_central_pressures(converted_parameters): # I might need additional inputs, such as added_keys. 
+        original_keys = list(converted_parameters.keys())
+
     if 'central_pressure_1' in converted_parameters.keys(): # There should be an analogous if statement for pc2. 
         mass_1_source = lalsim_SimNeutronStarMassconverted parameters['(central_pressure_1'], family,) # At this point, I think the source frame mass_1 has been found. 
         converted_parameters['redshift'] =\ # This is the beginning of the process for converting from source masses to detector frame masses, based on the process of going from detector frame masses to source frame masses onl lines 2203-2227 of the main branch version in Les' fork. 
             luminosity_distance_to_redshift(output_sample['luminosity_distance'])
         converted_parameters ['mass_1'] =\
             mass_1_source * (1 + converted_parameters['redshift'])
+        added_keys = [key for key in converted_parameters.keys()
+                      if key not in original_keys]
+    
+        return converted_parameters, added_keys
 
 
     if 'central_pressure_2' in converted_parameters.keys(): # There should be an analogous if statement for pc2. 
@@ -300,6 +306,11 @@ def generate_component_masses_from_central_pressures(converted_parameters): # I 
             luminosity_distance_to_redshift(output_sample['luminosity_distance'])
         converted_parameters ['mass_2'] =\
                 mass_2_source * (1 + converted_parameters['redshift'])
+        added_keys = [key for key in converted_parameters.keys()
+                      if key not in original_keys]
+    
+        return converted_parameters, added_keys
+
 
 
 
