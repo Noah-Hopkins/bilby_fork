59fa207277c417ca23a7dfe1c2154645c5a72d34
Noah Hopkins <hopkins1@kenyon.edu>
2025-07-02 14:41:43 -0400

    Added docstring to generate_component_masses_from_central_pressures.

--- bilby/gw/conversion.py
+++ bilby/gw/conversion.py
@@ -289,6 +289,38 @@ def convert_to_lal_binary_black_hole_parameters(parameters):
 
 
 def generate_component_masses_from_central_pressures(converted_parameters, added_keys, eos = 'placeholder'): 
+    """
+    Takes in converted_parameters array, added_keys list of any keys previously added to 
+    converted_parameters, and equation of state. Calculates source frame and detector frame 
+    component masses based component central pressures if component central pressures are 
+    present. Requires luminosity_distance to be in converted_parameters for the calculation 
+    of detector frame component masses to work. Does nothing if all inputs are provided, 
+    but component central pressures are not in converted_parameters. If no eos is provided, 
+    then the function will be exited around the beginning after printing a message about 
+    the issue. 
+
+    Input central pressures are expected in pascals. Output masses are in solar masses. 
+    
+    Parameters
+    ----------
+    converted_parameters: dict
+        dictionary of parameter values that the function uses to produce component masses if luminosity_distance 
+        and central pressures are included. 
+    added_keys: list
+        keys which were added to converted_parameters during the operation of previous functions
+    eos: lalsim swig-wrapped eos object
+        the neutron star equation of state
+    
+    Returns
+    -------
+    converted_parameters: dict
+        dictionary of the required parameters, now including source frame and detector frame component masses
+        along with redshift if there were component central pressures going in
+    added_keys: list
+        keys which are added to converted_parameters during function call
+
+    """
+
     original_keys = list(converted_parameters.keys())
     
     if eos == 'placeholder': 
