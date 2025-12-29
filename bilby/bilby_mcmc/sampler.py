bc81daaaf7ac705e1360e419da18ed2cdf8b11a9
Noah Scott Hopkins <hopkins1@gimli.kenyon.edu>
2025-12-29 15:56:27 -0500

    Added print statements to investigate this issue with swapping to final cold chain.

--- bilby/bilby_mcmc/sampler.py
+++ bilby/bilby_mcmc/sampler.py
@@ -846,6 +846,7 @@ class BilbyPTMCMCSampler(object):
             Eindexs = [0]
         for Eindex in Eindexs:
             for Tindex in range(self.ntemps - 1):
+                print(f"Tindex here is {Tindex}")
                 sampleri = self.sampler_dictionary[Tindex][Eindex]
                 vi, logli = self._get_sample_to_swap(sampleri)
                 betai = sampleri.beta
@@ -857,6 +858,7 @@ class BilbyPTMCMCSampler(object):
                 dbeta = betaj - betai
                 with np.errstate(over="ignore"):
                     alpha_swap = np.exp(dbeta * (logli - loglj))
+                    print(f"alpha_swap for temperature this time is {alpha_swap}")
 
                 if random.rng.uniform(0, 1) <= alpha_swap:
                     sampleri.chain[-1] = vj
@@ -869,6 +871,7 @@ class BilbyPTMCMCSampler(object):
 
     def swap_ensemble_chains(self):
         for Eindex in range(self.nensemble):
+            print(f"Eindex is {Eindex}")
             sampleri = self.sampler_dictionary[0][Eindex]
             vi, logli = self._get_sample_to_swap(sampleri)
 
@@ -877,12 +880,17 @@ class BilbyPTMCMCSampler(object):
 
             with np.errstate(over="ignore"):
                 alpha_swap = np.exp(logli - loglj)
+                print(f"alpha_swap for ensemble this time is {alpha_swap}")
+                print(f"This chain's sample was {vi}. The previous chain's sample was {vj}. ")
 
             if random.rng.uniform(0, 1) <= alpha_swap:
                 sampleri.chain[-1] = vj
                 samplerj.chain[-1] = vi
                 self.sampler_dictionary[0][Eindex] = sampleri
                 self.sampler_dictionary[0][Eindex - 1] = samplerj
+                #if (Eindex == self.nensemble-1): 
+                    #print("chad")
+                    #print(f"Last chain's sample is now {vj}. Second last chain's sample is now {vi}. ")
                 sampleri.pt_accepted += 1
             else: 
                 sampleri.pt_rejected += 1
