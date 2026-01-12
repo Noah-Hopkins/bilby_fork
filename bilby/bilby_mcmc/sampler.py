7d447053bdad5bff630d61ffac48e7ca2f621894
Noah Scott Hopkins <hopkins1@gimli.kenyon.edu>
2026-01-12 15:31:18 -0500

    Commented out the extra testing print statements.

--- bilby/bilby_mcmc/sampler.py
+++ bilby/bilby_mcmc/sampler.py
@@ -846,7 +846,7 @@ class BilbyPTMCMCSampler(object):
             Eindexs = [0]
         for Eindex in Eindexs:
             for Tindex in range(self.ntemps - 1):
-                print(f"Tindex here is {Tindex}")
+                #print(f"Tindex here is {Tindex}")
                 sampleri = self.sampler_dictionary[Tindex][Eindex]
                 vi, logli = self._get_sample_to_swap(sampleri)
                 betai = sampleri.beta
@@ -858,7 +858,7 @@ class BilbyPTMCMCSampler(object):
                 dbeta = betaj - betai
                 with np.errstate(over="ignore"):
                     alpha_swap = np.exp(dbeta * (logli - loglj))
-                    print(f"alpha_swap for temperature this time is {alpha_swap}")
+                    #print(f"alpha_swap for temperature this time is {alpha_swap}")
 
                 if random.rng.uniform(0, 1) <= alpha_swap:
                     sampleri.chain[-1] = vj
@@ -872,7 +872,7 @@ class BilbyPTMCMCSampler(object):
     def swap_ensemble_chains(self):
         for Eindex in range(self.nensemble):
             if (Eindex != 0):
-                print(f"Eindex is {Eindex}")
+                #print(f"Eindex is {Eindex}")
                 sampleri = self.sampler_dictionary[0][Eindex]
                 vi, logli = self._get_sample_to_swap(sampleri)
 
@@ -881,8 +881,8 @@ class BilbyPTMCMCSampler(object):
 
                 with np.errstate(over="ignore"):
                     alpha_swap = np.exp(loglj - logli)
-                    print(f"alpha_swap for ensemble this time is {alpha_swap}")
-                    print(f"This chain's sample was {vi}. The previous chain's sample was {vj}. ")
+                    #print(f"alpha_swap for ensemble this time is {alpha_swap}")
+                    #print(f"This chain's sample was {vi}. The previous chain's sample was {vj}. ")
 
                 if random.rng.uniform(0, 1) <= alpha_swap:
                     sampleri.chain[-1] = vj
